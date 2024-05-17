c32ce282ea6ac26f346b58dae6eff3672c2e28ed
= <=>
2024-05-18 00:45:28 +0530

    moved SiteHeader component to layout

--- leet-board/src/components/main-nav.tsx
+++ leet-board/src/components/main-nav.tsx
@@ -4,12 +4,14 @@ import Link from "next/link"
 import { NavItem } from "@/types/nav"
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
+import {useRouter} from "next/navigation"
 
 interface MainNavProps {
   items?: NavItem[]
 }
 
 export function MainNav({ items }: MainNavProps) {
+  const router = useRouter()
   return (
     <div className="flex gap-6 md:gap-10">
       <Link href="/" className="flex items-center space-x-2">
@@ -23,11 +25,11 @@ export function MainNav({ items }: MainNavProps) {
               item.href && (
                 <Link
                   key={index}
-                  href={item.href}
+                  href={'/problems'}
                   className={cn(
-                    "flex items-center text-sm font-medium text-muted-foreground",
-                    item.disabled && "cursor-not-allowed opacity-80"
+                    "flex items-center text-sm font-medium "
                   )}
+                  onClick = {()=>router.push('/problems')}
                 >
                   {"Problems"}
                 </Link>
