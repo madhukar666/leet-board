6c860c5c68389b70838725758d54bbe19fc4cc60
= <=>
2024-05-18 01:02:11 +0530

    add problems card

---
+++ leet-board/src/app/problems/page.tsx
@@ -0,0 +1,12 @@
+
+
+import {ProblemForm } from "@/components/custom/lc-form"
+
+export default function ProblemCard(){
+
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <ProblemForm></ProblemForm>
+    </div>
+  )
+}
