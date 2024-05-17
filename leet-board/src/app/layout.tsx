2e6d4d366d5a21b1671fc08d487e1e1c981cf5cd
= <=>
2024-05-18 00:49:27 +0530

    add problems card

--- leet-board/src/app/layout.tsx
+++ leet-board/src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
+import { SiteHeader } from "@/components/header"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,6 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <SiteHeader></SiteHeader>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
