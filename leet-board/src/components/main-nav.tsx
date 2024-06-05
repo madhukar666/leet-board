46aea3f42454f1cbae8ce2ad89eda8eab47a9c0a
= <=>
2024-06-06 01:26:04 +0530

    Add logo

--- leet-board/src/components/main-nav.tsx
+++ leet-board/src/components/main-nav.tsx
@@ -3,7 +3,9 @@ import Link from "next/link"
 import { NavItem } from "@/types/nav"
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
+import Image from "next/image";
 
+import logo from "../../public/logo.png"
 
 interface MainNavProps {
   items?: NavItem[]
@@ -13,9 +15,12 @@ export function MainNav({ items }: MainNavProps) {
 
   return (
     <div className="flex gap-6 md:gap-10">
+        <div className="flex flex-col md:flex-row md:gap-1">
+        <Link href="/"><Image className={"w-16"} src={logo} alt={"logo"}/></Link>
       <Link href="/" className="flex items-center space-x-2">
         <span className="inline-block font-bold">{siteConfig.name}</span>
       </Link>
+            </div>
       {items?.length ? (
         <nav className="flex gap-6">
           {items?.map(
